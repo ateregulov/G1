3735c8d3557a2422addcdf5bce404aec64af3fc6
Teregulov A.R <teregulov@mseb.ru>
2013-12-17 16:40:45 +0400

    init

---
+++ G1/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(G1.Startup))]
+namespace G1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
